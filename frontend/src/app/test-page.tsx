c60f6127cf0fe97c3f5cd20ebfc65e2fe8087f89
Sahil 2006 <sahilshabbirharyani@gmail.com>
2025-09-14 12:33:42 +0300

    perfected for deploymnet

--- frontend/src/app/test-page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div>
-      <h1>Test Page - If you see this, the app is working!</h1>
-      <p>CoCred is deployed successfully</p>
-    </div>
-  );
-}

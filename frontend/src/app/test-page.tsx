e24d9245d24ba3f897ec2dd95be5c1f1561d7c9d
Sahil 2006 <sahilshabbirharyani@gmail.com>
2025-09-14 12:27:36 +0300

    Simplify homepage for testing

---
+++ frontend/src/app/test-page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Test Page - If you see this, the app is working!</h1>
+      <p>CoCred is deployed successfully</p>
+    </div>
+  );
+}
